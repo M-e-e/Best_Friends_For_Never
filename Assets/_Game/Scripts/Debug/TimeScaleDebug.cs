95b84f9fe730f81dff7c8a76a2556e59eca21dab
M-e-e <44639352+M-e-e@users.noreply.github.com>
2019-10-01 15:41:06 +0200

    Pause Menu actually pauses the game

---
+++ Assets/_Game/Scripts/Debug/TimeScaleDebug.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaleDebug : MonoBehaviour
+{
+	private bool stop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (Input.GetKeyDown(KeyCode.P))
+	    {
+		    stop = !stop;
+
+		    Time.timeScale = stop ? 0 : 1;
+	    }
+    }
+}
