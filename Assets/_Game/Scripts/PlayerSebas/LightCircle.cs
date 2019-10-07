aa21100c00cad2d4d9783c6bfc85c8548c8da29b
Dreale-thm <55383237+Dreale-thm@users.noreply.github.com>
2019-10-07 16:00:55 +0200

    Reworked Lighting on Players - need to implement Changes in Level 1-9

--- Assets/_Game/Scripts/PlayerSebas/LightCircle.cs
+++ Assets/_Game/Scripts/PlayerSebas/LightCircle.cs
@@ -13,28 +13,52 @@ public class LightCircle : MonoBehaviour
 	[SerializeField] private FloatConstant DistanceBeforeDeath;
 	[SerializeField] private FloatConstant MaxRadius;
 	[SerializeField] private FloatConstant MinRadius;
-
+	[SerializeField] private StringConstant BigLight;
 	public float LightRadiusModifier = 0;
 
-	private Light2D _light;
+	private Light2D _light, _bLight;
+	[SerializeField] private GameObject _bigLightGameObject;
 
-    // Start is called before the first frame update
+	// Start is called before the first frame update
     void Start()
     {
 	    _light = GetComponent<Light2D>();
+	    //_bigLightGameObject = AtomicTags.FindByTag(BigLight.Value);
+
+	    _bLight = _bigLightGameObject.GetComponent<Light2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-	    _light.intensity = Mathf.Min(0.55f,Mathf.Max(((TimeBeforeDeath.Value - TimeSinceTouch.Value) / TimeBeforeDeath.Value  / 1.5f),0.2f));
-		Debug.Log(_light.intensity);
+	    Debug.Log(("script alive"));
+	    float intensity = Mathf.Min(0.55f,Mathf.Max(((TimeBeforeDeath.Value - TimeSinceTouch.Value) / TimeBeforeDeath.Value  / 1.5f),0.2f));
+	    float LightRadius = Mathf.Max((DistanceBeforeDeath.Value - CurrentPlayerDistance.Value) / DistanceBeforeDeath.Value * (MaxRadius.Value + LightRadiusModifier),MinRadius.Value);
 
-	    float LightRadius = (DistanceBeforeDeath.Value - CurrentPlayerDistance.Value) / DistanceBeforeDeath.Value * (MaxRadius.Value + LightRadiusModifier);
-	    if (LightRadius > MinRadius.Value)
-	    {
-		    _light.pointLightOuterRadius = LightRadius;
-	    }
+		//Debug.Log(_light.intensity);
+		//Debug.Log("Distance: " +CurrentPlayerDistance.Value);
+		if (CurrentPlayerDistance.Value < 1)
+		{
+
+			_bigLightGameObject.SetActive(true);
+			_light.transform.gameObject.SetActive(false);
+			_bLight.intensity = intensity * 2;
+			_bLight.pointLightOuterRadius = LightRadius;
+
+		}
+		else
+		{
+			_bigLightGameObject.SetActive(false);
+			_light.transform.parent.gameObject.SetActive(true);
+			_light.intensity = intensity;
+			_light.pointLightOuterRadius = LightRadius;
+
+
+
+
+		}
+
+
 
     }
 }
